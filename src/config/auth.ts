207bd50a321f85524676e1f2a7fe615d32fad2d7
Henrique Batista <henrique.batista@epoca.dc>
2020-04-19 10:48:10 -0300

    Feat: Autenticação com Token JWT

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '91e9c48c88eacf7d57c7a31cafef6ca5',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import appotimentsRouter from './appointments.routes';
+
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
-routes.use('/appointments', appotimentsRouter);
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
